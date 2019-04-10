25d133f48b1672b705caa715790d6b117c2d54fc
Amresh <amreshyadav09@gmail.com>
2019-04-10 15:00:05 +0530

    Updated the program

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld
 {
 public static void main(String args[])
 {
-System.out.println("Hello WORLD");
+System.out.println("Hello WORLD1");
+System.out.println("Hello WORLD2");
 }
 }
